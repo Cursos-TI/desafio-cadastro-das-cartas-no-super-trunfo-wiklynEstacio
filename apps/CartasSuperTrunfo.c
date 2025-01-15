6b07b308cebe2798d17bcbe94632f2500a4c6c66
wiklynEstacio <202402751999@alunos.estacio.br>
2025-01-15 04:28:57 +0000

    Adiciona a implementacao da funcao main no arquivo principal

--- apps/CartasSuperTrunfo.c
+++ apps/CartasSuperTrunfo.c
@@ -1,22 +1,48 @@
-#include <stdio.h>
+#include "city.h"
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// O jogo original possui 32 cartas
+#define MAX_CITIES 32
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+int main()
+{
+    // Cria uma lista de ponteiros nulos, onde serão apontadas as cartas
+    City* city_list[MAX_CITIES] = {NULL};
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    // Adiciona variáveis de apoio
+    int registered_cities_count = 0;
+    char keep_registering = 'S';
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Mantém interação com usuário até o número máximo de cartas ser registrado
+    // ou até o usuário responder que não quer mais registrar nenhuma carta.
+    while (registered_cities_count < MAX_CITIES && keep_registering == 'S')
+    {
+        // Cria um ponteiro para ter acesso aos dados enviados pelo usuário
+        City* city = get_city_data();
 
+        // Usa um dos ponteiros da lista de cidades para apontar para os dados
+        // enviados pelo usuário
+        register_city(city, city_list, registered_cities_count);
+
+        puts("\nCarta cadastrada com sucesso.\n");
+
+        // Exibe os dados enviados pelo usuário
+        print_city(city_list[registered_cities_count]);
+
+        registered_cities_count++;
+
+        // Libera o ponteiro `city`
+        free_city(&city);
+
+        // Pergunta ao usuário se deve continuar a registrar cartas
+        if (registered_cities_count < MAX_CITIES) {
+            keep_registering = ask_to_keep_registering() ? 'S' : 'N';
+        } else {
+            puts("\nNumero maximo de cartas atingido.");
+        }
+    }
+
+    // Libera o espaço na memória alocado para as cidades
+    free_city_list(city_list, registered_cities_count);
+    
     return 0;
 }
