8c45132bd5ee8a21183239e83435983a1a497fae
wiklynEstacio <202402751999@alunos.estacio.br>
2025-01-15 04:26:58 +0000

    Adiciona a implementacao de todas as funcoes declaradas no cabecalho

---
+++ src/city.c
@@ -0,0 +1,152 @@
+#include "city.h"
+
+City* get_city_data()
+{
+    char provided_state;
+    char provided_card_code[4];
+    char provided_city_name[256];
+    
+    City *city = (City*) calloc(1, sizeof(City));
+    
+    if (city == NULL) {
+        perror("Erro ao alocar memoria para a cidade em `get_city_data`\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Digite o estado: ");
+    scanf(" %c", &provided_state);
+    getchar();
+    provided_state = toupper(provided_state);
+    city->state = provided_state;
+
+    printf("Digite o codigo da carta: ");
+    scanf("%3s", provided_card_code);
+    getchar();
+    provided_card_code[0] = toupper(provided_card_code[0]);
+    city->card_code = calloc(strlen(provided_card_code) + 1, sizeof(char));
+    if (city->card_code == NULL) {
+        perror("Erro ao alocar memoria para o codigo da carta");
+        exit(EXIT_FAILURE);
+    }
+    strcpy(city->card_code, provided_card_code);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%255s", provided_city_name);
+    getchar();
+    city->city_name = calloc(strlen(provided_city_name) + 1, sizeof(char));
+    if (city->city_name == NULL) {
+        perror("Erro ao alocar memória para o nome da cidade");
+        exit(EXIT_FAILURE);
+    }
+    strcpy(city->city_name, provided_city_name);
+
+    printf("Digite a populacao: ");
+    scanf("%d", &city->population);
+    getchar();
+
+    printf("Digite a area: ");
+    scanf("%f", &city->area);
+    getchar();
+
+    printf("Digite o PIB: ");
+    scanf("%f", &city->gpd);
+    getchar();
+ 
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &city->tourist_sites_count);
+    getchar();
+
+    return city;
+}
+
+void register_city(City* city, City** city_list, int registered_cities_count)
+{
+    if (city == NULL) {
+        fprintf(stderr, "Erro: ponteiro NULL fornecido para `register_city`.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    city_list[registered_cities_count] = (City*) calloc(1, sizeof(City));
+    
+    if (city_list[registered_cities_count] == NULL) {
+        perror("Erro ao alocar memoria para uma cidade em `register_city`.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    city_list[registered_cities_count]->state = city->state;
+
+    city_list[registered_cities_count]->card_code = calloc(3, sizeof(char));
+    strcpy(city_list[registered_cities_count]->card_code, city->card_code);
+
+    city_list[registered_cities_count]->city_name = calloc(
+        (strlen(city->city_name)), sizeof(char)
+    );
+    if (city_list[registered_cities_count]->city_name == NULL) {
+        perror("Erro ao alocar memoria para o nome da cidade em `register_city`.\n");
+        exit(EXIT_FAILURE);
+    }
+    strcpy(city_list[registered_cities_count]->city_name, city->city_name);
+
+    city_list[registered_cities_count]->population = city->population;
+    city_list[registered_cities_count]->area = city->area;
+    city_list[registered_cities_count]->gpd = city->gpd;
+    city_list[registered_cities_count]->tourist_sites_count = city->tourist_sites_count;
+}
+
+void print_city(const City *city)
+{
+    if (city == NULL) {
+        fprintf(stderr, "Erro: ponteiro NULL fornecido para `print_city`.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Estado: %c\n", city->state);
+    printf("Codigo da Carta: %s\n", city->card_code);
+    printf("Nome da Cidade:: %s\n", city->city_name);
+    printf("Populacao: %d\n", city->population);
+    printf("Area: %.2f km²\n", city->area);
+    printf("PIB: %.2f bilhoes de reais\n", city->gpd);
+    printf("Numero de Pontos Turisticos: %d\n", city->tourist_sites_count);
+}
+
+void free_city(City** city_ref)
+{
+    if (city_ref == NULL)
+        return;
+
+    if (*city_ref == NULL)
+        return;
+
+    City *city = *city_ref;
+    free(city->card_code);
+    free(city->city_name);
+    free(city);
+    *city_ref = NULL;
+}
+
+bool ask_to_keep_registering()
+{
+    char response;
+    
+    do {
+        printf("\nRegistrar outra carta? (S/N): ");
+        scanf(" %c", &response);
+        getchar();
+        puts("");
+        response = toupper(response);
+
+        if (response != 'S' && response != 'N') {
+            puts("Entrada invalida. Por favor, digite 'S' para sim ou 'N' para nao.");
+        }
+    } while (response != 'S' && response != 'N');
+
+    return response == 'S';
+}
+
+void free_city_list(City** city_list, int registered_cities_count)
+{
+    for (int i = 0; i < registered_cities_count; i++) {
+        if (city_list[i] != NULL)
+            free_city(&city_list[i]);
+    }
+}
