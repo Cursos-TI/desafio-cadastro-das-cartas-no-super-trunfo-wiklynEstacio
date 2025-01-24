b1c4905a2f693443af02d059b21a6e78703709d9
wiklynEstacio <202402751999@alunos.estacio.br>
2025-01-24 09:10:45 +0000

    Adiciona a funcao que executa o fluxo de registro de cartas

--- src/card.c
+++ src/card.c
@@ -27,3 +27,62 @@ int choose_functionality()
     puts("");
     return chosen_functionality;
 }
+
+void register_cards(int max_cities)
+{
+    if (max_cities <= 0)
+    {
+        fprintf(stderr, "Erro: valor de `max_cites` precisa ser positivo.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Cria uma lista de ponteiros nulos, onde serão apontadas as cartas
+    City* city_list[max_cities];
+    for (int i = 0; i < max_cities; i++) {
+        city_list[i] = NULL;
+    }
+    
+    // Adiciona variáveis de apoio
+    int registered_cities_count = 0;
+    char keep_registering = 'S';
+    
+    // Mantém interação com usuário até o número máximo de cartas ser registrado
+    // ou até o usuário responder que não quer mais registrar nenhuma carta.
+    while (registered_cities_count < max_cities && keep_registering == 'S')
+    {
+        // Cria um ponteiro para ter acesso aos dados enviados pelo usuário
+        City* city = get_city_data();
+
+        city_list[registered_cities_count] = (City*) calloc(1, sizeof(City));
+
+        // Usa um dos ponteiros da lista de cidades para apontar para os dados
+        // enviados pelo usuário
+        register_city(city, city_list[registered_cities_count]);
+
+        puts("\nCarta cadastrada com sucesso.\n");
+
+        // Exibe os dados enviados pelo usuário
+        print_city(city_list[registered_cities_count]);
+
+        registered_cities_count++;
+
+        // Libera o ponteiro `city`
+        free_city(&city);
+
+        // Pergunta ao usuário se deve continuar a registrar cartas
+        if (registered_cities_count < max_cities)
+        {
+            if (ask_to_keep_registering())
+            {
+                keep_registering = 'S';
+                puts("");
+            }
+            else
+                keep_registering = 'N';
+        } else
+            puts("\nNumero maximo de cartas atingido.");
+    }
+
+    // Libera o espaço na memória alocado para as cidades
+    free_city_list(city_list, registered_cities_count);
+}
