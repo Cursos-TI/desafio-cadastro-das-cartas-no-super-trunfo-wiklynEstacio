8f5a2c128588dba6ceaa17f576247bb441370425
wiklynEstacio <202402751999@alunos.estacio.br>
2025-01-24 09:11:14 +0000

    Adiciona a funcao que executa o fluxo de comparacao de cartas

--- src/card.c
+++ src/card.c
@@ -86,3 +86,86 @@ void register_cards(int max_cities)
     // Libera o espaço na memória alocado para as cidades
     free_city_list(city_list, registered_cities_count);
 }
+
+void compare_cards()
+{
+    // Aloca um array com dois ponteiros nulos
+    City* city_list[2];
+    for (int i = 0; i < 2; i++) {
+        city_list[i] = NULL;
+    }
+
+    // Aponta os ponteiros nulos para estruturas `City` 
+    city_list[0] = alocate_city();
+    city_list[1] = alocate_city();
+
+    // Coleta os dados para as estruturas `City`. Também copia os dados
+    // fornecidos para a lista de cidades e calcula valores extras através
+    // desses dados
+    City *first_city = get_one_city_data("primeira");
+    register_city(first_city, city_list[0]);
+
+    puts("");
+
+    City *second_city = get_one_city_data("segunda");
+    register_city(second_city, city_list[1]);
+
+    // Exibe os dados coletados e calculados no terminal
+    print_card_to_compare(city_list[0], 1);
+    print_card_to_compare(city_list[1], 2);
+
+    puts("\nComparando as cartas: Caso a carta 1 seja a vencedora, o resultado será 1. Caso a carta 2 seja a vencedora, o resultado será 0.\n");
+
+    // Faz comparações entre as propriedades. Se a city_list[0] ganhar, exibe 1
+    // no terminal. Se a city_list[1] ganhar, exibe 0 no terminal
+    printf(
+        "Qual carta tem maior populacao? %d. Portanto, a carta %d ganhou.",
+        city_list[0]->population_size > city_list[1]->population_size,
+        city_list[0]->population_size > city_list[1]->population_size ? 1 : 2
+    );
+    puts("");
+
+    printf(
+        "Qual carta tem maior area? %d. Portanto, a carta %d ganhou.",
+        city_list[0]->area > city_list[1]->area,
+        city_list[0]->area > city_list[1]->area ? 1 : 2
+    );
+    puts("");
+
+    // Para a densidade pouplacional é ao contrário. Se a city_list[0] ganhar,
+    // exibe 0 no terminal. Se a city_list[1] ganhar, exibe 1 no terminal
+    printf(
+        "Qual carta tem menor densidade populacional? %d. Portanto, a carta %d ganhou.",
+        city_list[0]->population_density < city_list[1]->population_density,
+        city_list[0]->population_density < city_list[1]->population_density ? 1 : 2
+    );
+    puts("");
+
+    printf(
+        "Qual carta tem maior PIB? %d. Portanto, a carta %d ganhou.",
+        city_list[0]->gpd > city_list[1]->gpd,
+        city_list[0]->gpd > city_list[1]->gpd ? 1 : 2
+    );
+    puts("");
+
+    printf(
+        "Qual carta tem maior PIB per Capta? %d. Portanto, a carta %d ganhou.",
+        city_list[0]->gpd_pc > city_list[1]->gpd_pc,
+        city_list[0]->gpd_pc > city_list[1]->gpd_pc ? 1 : 2
+    );
+    puts("");
+
+    printf(
+        "Qual carta tem maior numero de pontos turisticos? %d. Portanto, a carta %d ganhou.",
+        city_list[0]->tourist_sites_count > city_list[1]->tourist_sites_count,
+        city_list[0]->tourist_sites_count > city_list[1]->tourist_sites_count ? 1 : 2
+    );
+    puts("");
+
+    printf(
+        "Qual carta tem maior super poder? %d. Portanto, a carta %d ganhou.",
+        city_list[0]->super_power > city_list[1]->super_power,
+        city_list[0]->super_power > city_list[1]->super_power ? 1 : 2
+    );
+    puts("");
+}
